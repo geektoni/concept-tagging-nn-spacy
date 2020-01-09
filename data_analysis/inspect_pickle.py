2fb93f6b202a62382a1784a0e679eee72d34c243
Giovanni De Toni <giovanni.det@gmail.com>
2020-01-09 17:16:47 +0100

    Update inspect_pickle such to save an unpickled version to csv.

--- data_analysis/inspect_pickle.py
+++ data_analysis/inspect_pickle.py
@@ -3,7 +3,7 @@ import pickle
 
 parser = argparse.ArgumentParser(description='Inspect pickle files used for training/testing')
 parser.add_argument("filename", metavar='file', nargs=1, type=str, help="Path to the file we want to explore")
-
+parser.add_argument("--save", default=False, action="store_true", help="Save the file to a manageable csv format")
 args = parser.parse_args()
 
 objects = []
@@ -16,3 +16,7 @@ with (open(args.filename[0], "rb")) as openfile:
 
 print(objects[0].columns)
 print(objects)
+
+# Save pickle to csv file to be inspected
+if args.save:
+    objects[0].to_csv("saved_pickle.csv")
