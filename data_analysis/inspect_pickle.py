a937d641317b4bc277f2fb198a3bc69c7829766c
Giovanni De Toni <giovanni.det@gmail.com>
2020-01-14 14:41:40 +0100

    Add a on-the-fly convert ability to inspect pickle.

--- data_analysis/inspect_pickle.py
+++ data_analysis/inspect_pickle.py
@@ -1,8 +1,11 @@
 import argparse
 import pickle
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Inspect pickle files used for training/testing')
 parser.add_argument("filename", metavar='file', nargs=1, type=str, help="Path to the file we want to explore")
+parser.add_argument("--column", type=str, help="Print exactly this column")
+parser.add_argument("--quick-bert-convert", default=False, action="store_true", help="Quickly convert BERT to correct file")
 parser.add_argument("--save", default=False, action="store_true", help="Save the file to a manageable csv format")
 args = parser.parse_args()
 
@@ -17,9 +20,29 @@ with (open(args.filename[0], "rb")) as openfile:
 # Print the columns of the file(s) and its content
 for o in objects:
     print(o.columns)
-print(objects)
+
+if args.column:
+    for o in objects:
+        print(o[args.column])
+else:
+    print(objects)
+
+
+if args.quick_bert_convert:
+    total_tokens = []
+    for o in objects:
+        for index, row in o.iterrows():
+            raw_emb = []
+            for e in row["tokens_emb"]:
+                raw_emb.append(e[1][0])
+            total_tokens.append(raw_emb)
+    objects[0]["tokens_emb"] = np.array(total_tokens)
 
 # Save pickle to csv file(s) to be inspected
 if args.save:
     for index, o in enumerate(objects):
-        objects[0].to_csv("saved_pickle_{}.csv", index)
+        if not args.quick_bert_convert:
+            o.to_csv("saved_pickle_{}.csv".format(index))
+        else:
+            with open("saved_pickle_{}.pickle".format(index), "wb") as output:
+                pickle.dump(o, output)
