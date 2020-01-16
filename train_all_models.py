0a6338d59e32500910fb72c827c0eeb341c44c86
Giovanni De Toni <giovanni.det@gmail.com>
2020-01-16 19:03:40 +0100

    Update train all model for the cluster

--- train_all_models.py
+++ train_all_models.py
@@ -5,23 +5,22 @@ import random
 # set random seed
 random.seed(42)
 
-max_proc = 50
+max_proc = 25
 
 emb = [
-    ("none", "../../data/train_updated.pickle", "../../data/test_updated.pickle", "../data/movies/w2v_trimmed.pickle",
+    ("none", "../../data/train.bz2", "../../data/test.bz2", "../data/movies/w2v_trimmed.bz2",
      "../data/movies/c2v_20.pickle"),
-    ("elmo", "../../data/train_elmo.pickle", "../../data/test_elmo.pickle", "../data/movies/w2v_trimmed.pickle",
+    ("elmo", "../../data/train_elmo.bz2", "../../data/test_elmo.bz2", "../data/movies/w2v_trimmed.bz2",
      "../data/movies/c2v_20.pickle"),
-    ("bert", "../../data/train_bert.pickle", "../../data/test_bert.pickle", "../data/movies/w2v_trimmed.pickle",
+    ("elmo-combined", "../../data/train_elmo.bz2", "../../data/test_elmo.bz2", "../data/movies/w2v_trimmed.bz2",
      "../data/movies/c2v_20.pickle"),
-    #("glove", "../../data/train_updated.pickle", "../../data/test_updated.pickle", "../../data/embeddings/glove.6B.100d.txt.updated.pickle"),
-    #("glove_lg", "../../data/train_updated.pickle", "../../data/test_updated.pickle", "../../data/embeddings/glove.6B.100d.txt.updated.pickle")
+    ("bert", "../../data/train_bert.bz2", "../../data/test_bert.bz2", "../data/movies/w2v_trimmed.bz2",
+     "../data/movies/c2v_20.pickle"),
+    ("glove", "../../data/train.bz2", "../../data/test.bz2", "../../data/embeddings/glove.6B.100d.txt.updated.bz2"),
 ]
 
 configs = [
-    ["lstm", 200, 15, 20, 0.001, 0.70, 6],
-    ["lstm2ch", 200, 20, 15, 0.001, 0.30, 8],
-    ["lstmcrf", 200, 10, 1, 0.001, 0.70, 6]
+    ["lstm", 200, 15, 20, 0.001, 0.70, 6]
 ]
 
 for c in configs:
@@ -37,9 +36,9 @@ for c in configs:
                 file_name = "result"
 
                 # Double the iterations if we are using a different embedding
-                iterations=c[2]
-                if e == "bert" or e == "elmo":
-                    iterations=c[2]*2
+                #iterations=c[2]
+                #if e == "bert" or e == "elmo" or e == "elmo-combined":
+                #    iterations=c[2]*2
 
                 # If we are using chars, then we double the size of the hidden layer
                 # in the case of the lstm
@@ -49,7 +48,7 @@ for c in configs:
 
                 # Generate the command
                 command = "bash submit_jobs.sh {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}".format(
-                    c[0], hidden_size, iterations, c[3], c[4], c[5], c[6], e[0], f, e[1], e[2], e[3], file_name,
+                    c[0], hidden_size, c[2], c[3], c[4], c[5], c[6], e[0], f, e[1], e[2], e[3], file_name,
                     random.randint(0, 1000000), max_proc, charemb, e[4]
                 )
 
