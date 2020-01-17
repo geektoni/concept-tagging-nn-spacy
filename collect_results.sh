e018368585a430686c2ecaf93a393ed097185a2c
Giovanni De Toni <giovanni.det@gmail.com>
2020-01-17 10:40:24 +0100

    Skip if empty dir.

--- collect_results.sh
+++ collect_results.sh
@@ -2,7 +2,11 @@
 
 for f in `ls ./results/`;
 do
-  python concept-tagging-with-neural-networks/src/collect_results.py ./results/${f}
-  result=`cat ./results/${f}/f1.scores | head -n 1`
-  echo $f, $result >> complete_results.txt
+  if [ ! -z "$(ls -A ./results/${f})" ]; then
+    cd concept-tagging-with-neural-networks/src/
+    python collect_results.py ../../results/${f}
+    result=`cat ../../results/${f}/f1.scores | head -n 1`
+    echo $f, $result >> ../../complete_results.txt
+    cd ../../
+  fi
 done
