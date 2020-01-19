c92a0717ebdee2cf7b9a8167bdc055e03be4f9b8
Giovanni De Toni <giovanni.det@gmail.com>
2020-01-19 14:34:20 +0100

    Update complete results file.

--- collect_results.sh
+++ collect_results.sh
@@ -5,8 +5,11 @@ do
   if [ ! -z "$(ls -A ./results/${f})" ]; then
     cd concept-tagging-with-neural-networks/src/
     python collect_results.py ../../results/${f}
-    result=`cat ../../results/${f}/f1.scores | head -n 1`
-    echo $f, $result >> ../../complete_results.txt
+    mean=`cat ../../results/${f}/f1.scores | head -n 1 | cut -d"," -f 1 -- | cut -d":" -f 2 | awk '{$1=$1;print}'`
+    std=`cat ../../results/${f}/f1.scores | head -n 1 | cut -d"," -f 2 -- |  cut -d":" -f 2 | awk '{$1=$1;print}'`
+    max=`cat ../../results/${f}/f1.scores | head -n 1 | cut -d"," -f 3 -- |  cut -d":" -f 2 | awk '{$1=$1;print}'`
+    min=`cat ../../results/${f}/f1.scores | head -n 1 | cut -d"," -f 4 -- |  cut -d":" -f 2 | awk '{$1=$1;print}'`
+    echo $f, $mean, $std, $max, $min >> ../../complete_results.txt
     cd ../../
   fi
 done
